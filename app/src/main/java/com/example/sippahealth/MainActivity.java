a50089a9504a383ae00acc0f15acfdbad35a4d52
brhwang0 <brhwang@protonmail.com>
2020-07-23 20:41:03 -0400

    change target to 28, revert to Android legacy dependencies

--- app/src/main/java/com/example/sippahealth/MainActivity.java
+++ app/src/main/java/com/example/sippahealth/MainActivity.java
@@ -1,14 +1,12 @@
 package com.example.sippahealth;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
-
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.view.WindowManager;
-
-import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
--- app/src/main/java/com/example/sippahealth/SippaStore.java
+++ app/src/main/java/com/example/sippahealth/SippaStore.java
@@ -1,8 +1,7 @@
 package com.example.sippahealth;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class SippaStore extends AppCompatActivity {
 
